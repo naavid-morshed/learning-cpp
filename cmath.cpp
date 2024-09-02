713b97929d6d5e98fe73d58ce1f08d79730ad2fe
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-09-02 11:36:50 +0600

    learned about header files, cmath

---
+++ cmath.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << pow(2, 3) << std::endl;
+    std::cout << sqrt(9) << std::endl;
+    std::cout << std::max(2 ,3) << std::endl;
+    std::cout << std::min(2 ,3) << std::endl;
+}
--- getline.cpp
+++ getline.cpp
@@ -2,11 +2,17 @@
 
 int main() {
     std::string name;
+    int age;
+
+    std::cout << "Enter your age : ";
+    std::cin >> age; // cin puts a white space character after taking input, 
+    // it needs to be expelled otherwise it will be taken as input by getline
 
     std::cout << "Enter your full name : ";
-    std::getline(std::cin, name);
+    std::getline(std::cin >> std::ws, name);
 
-    std::cout << "Your Name is : " << name << std::endl;
-    
+    std::cout << "Hello : " << name << std::endl;
+    std::cout << "You are " << age << " years old." << std::endl;
+
     return 0;
 }
