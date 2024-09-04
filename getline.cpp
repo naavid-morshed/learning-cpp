f1cbe8a10ad44ee3d131c0a053b9b980c9354246
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-09-04 15:57:20 +0600

    minor fix, updated getline.cpp

--- getline.cpp
+++ getline.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     std::string name;
     int age;
 
     std::cout << "Enter your age : ";
-    std::cin >> age; // cin puts a white space character after taking input, 
+    std::cin >> age; // cin puts a white space character after taking input,
     // it needs to be expelled otherwise it will be taken as input by getline
 
     std::cout << "Enter your full name : ";
@@ -14,5 +15,13 @@ int main() {
     std::cout << "Hello : " << name << std::endl;
     std::cout << "You are " << age << " years old." << std::endl;
 
-    return 0;
+    std::string arr[] = {"Naavid", "", "Morshed"};
+
+    for (int i = 0; i < sizeof(arr) / sizeof(std::string); i++)
+    {
+        if (!arr[i].empty())
+        {
+            std::cout << arr[i] << std::endl;
+        }
+    }
 }
