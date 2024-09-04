cb8158b9950302751557da99680562652d72afb2
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-09-04 16:19:08 +0600

    learned about 2d array, trickier than 2d array in java

---
+++ 2d-array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    // [row][col]
+    // col must be specified
+    std::string arr[][3] = {
+        {"A", "B", "C"},
+        {"D", "E", "F"},
+        {"G", "H", "I"},
+        {"J", "K", "L"},
+    };
+
+    int rowSize = sizeof(arr) / sizeof(arr[0]);
+    int colSize = sizeof(arr[0]) / sizeof(arr[0][0]);
+
+    std::cout << "Row count : " << rowSize << "\n";
+    std::cout << "Col count : " << colSize << "\n";
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < colSize; j++)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << std::endl;
+}
