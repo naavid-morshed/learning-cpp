b9b86caa16862f725a18523a016b80df20f63bc2
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-09-12 12:14:16 +0600

    completed cpp basics, time to solve problems

--- class.cpp
+++ class.cpp
@@ -8,14 +8,17 @@ private:
     std::string department;
 
 public:
-    Student(std::string name, int id, std::string department)
+    Student() {}
+
+public:
+    Student(std::string name, int id, std::string department): name(name), id(id), department(department) // constructor initializer list
     {
-        // this->name = name;
-        // this->id = id;
-        // this->department = department;
-        this->setName(name); // both approaches are okay
-        this->setId(id);
-        this->setDepartment(department);
+        // // this->name = name;
+        // // this->id = id;
+        // // this->department = department;
+        // this->setName(name); // both approaches are okay
+        // this->setId(id);
+        // this->setDepartment(department);
     }
 
 public:
@@ -24,41 +27,66 @@ public:
         this->name = name;
     }
 
-public:
     void setId(int id)
     {
         this->id = id;
     }
-
-public:
     void setDepartment(std::string department)
     {
         this->department = department;
     }
 
-public:
     std::string getName()
     {
         return this->name;
     }
 
-public:
     int getId()
     {
         return this->id;
     }
 
-public:
     std::string getDepartment()
     {
         return this->department;
     }
 };
 
+class TeacherAssistant : public Student // has to be public otherwise inheritance will be private
+{
+private:
+    int salary;
+
+public:
+    TeacherAssistant(std::string name, int id, std::string department, int salary): Student(name, id, department), salary(salary)
+    {
+        // this->setName(name);
+        // this->setId(id);
+        // this->setDepartment(department);
+        // this->setSalary(salary);
+    }
+
+public:
+    void setSalary(int salary)
+    {
+        this->salary = salary;
+    }
+
+    int getSalary(){
+        return this->salary;
+    }
+};
+
 int main()
 {
     Student ss("Naavid Morshed", 18201196, "CSE");
-    std::cout << ss.getName() << std::endl;
-    std::cout << ss.getId() << std::endl;
-    std::cout << ss.getDepartment() << std::endl;
+    std::cout << ss.getName() << "\n";
+    std::cout << ss.getId() << "\n";
+    std::cout << ss.getDepartment() << "\n\n";
+
+    TeacherAssistant dd("Generic TA", 10001000, "CSE", 9000);
+    std::cout << dd.getName() << "\n";
+    std::cout << dd.getId() << "\n";
+    std::cout << dd.getDepartment() << "\n";
+    std::cout << dd.getSalary() << std::endl;
 }
